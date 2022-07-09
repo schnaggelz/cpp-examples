faf8120e8560da072ee8a578d18be2b02d8561b0
Timon Reich <timon.reich@gmx.de>
2022-07-09 20:04:16 +0200

    wip

---
+++ callback/include/callback.hpp
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (C) 2022 Timon Reich
+ *
+ * Exampls library
+ *
+ * License notes see LICENSE.txt
+ ******************************************************************************/
+
+#ifndef __EXAMPLES_CALLBACK_HPP__
+#define __EXAMPLES_CALLBACK_HPP__
+
+namespace examples
+{
+namespace callbacks
+{
+class Callback
+{
+};
+
+}  // namespace remote
+}  // namespace nxt
+
+#endif  // __EXAMPLES_CALLBACK_HPP__
---
+++ observer/include/observer.hpp
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (C) 2022 Timon Reich
+ *
+ * Exampls library
+ *
+ * License notes see LICENSE.txt
+ ******************************************************************************/
+
+#ifndef __EXAMPLES_OBSERVER_HPP__
+#define __EXAMPLES_OBSERVER_HPP__
+
+namespace examples
+{
+namespace observer
+{
+class Observer
+{
+};
+
+}  // namespace remote
+}  // namespace nxt
+
+#endif  // __EXAMPLES_OBSERVER_HPP__
