ea31e3290624cadf7376af0d41f722bc77c48650
Timon Reich <timon.reich@gmx.de>
2024-05-09 09:17:33 +0200

    wip

---
+++ mpsc/include/mpsc/ring.hpp
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (C) 2022 Timon Reich
+ *
+ * C++ examples library
+ *
+ * License notes see LICENSE.txt
+ ******************************************************************************/
+
+#ifndef __EXAMPLES_MPSC_RING_HPP__
+#define __EXAMPLES_MPSC_RING_HPP__
+
+namespace examples
+{
+namespace mpsc
+{
+class Ring
+{
+};
+
+}  // namespace mpsc
+}  // namespace examples
+
+#endif  // __EXAMPLES_MPSC_RING_HPP__
---
+++ mpsc/test/test_ring.cpp
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (C) 2022 Timon Reich
+ *
+ * C++ examples library
+ *
+ * License notes see LICENSE.txt
+ ******************************************************************************/
+
+#include "mpsc/ring.hpp"
+
+#include <gtest/gtest.h>
+
+struct TestFixture : testing::Test
+{
+};
+
+TEST_F(TestFixture, Sandbox)
+{
+
+}
